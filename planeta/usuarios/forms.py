733796e423f11b122da7f50c3140c67645a5d932
Moisés Cachay <xpktro@gmail.com>
2017-10-21 11:17:37 -0500

    Primer tercio

--- planeta/usuarios/forms.py
+++ planeta/usuarios/forms.py
@@ -5,3 +5,10 @@ from django.contrib.auth.forms import UserCreationForm
 class RegistroForm(UserCreationForm):
     nombres = forms.CharField(required=True)
     apellidos = forms.CharField(required=True)
+
+    def save(self, commit=True):
+        usuario = super(RegistroForm, self).save(commit=False)
+        usuario.first_name = self.cleaned_data['nombres']
+        usuario.last_name = self.cleaned_data['apellidos']
+        usuario.save()
+        return usuario
--- planeta/usuarios/views.py
+++ planeta/usuarios/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login
 from django.views.generic import FormView
 
 from usuarios.forms import RegistroForm
@@ -6,3 +7,18 @@ from usuarios.forms import RegistroForm
 class Registro(FormView):
     template_name = 'registro.html'
     form_class = RegistroForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        # data = form.cleaned_data
+        # usuario = User()
+        # usuario.first_name = data['nombres']
+        # usuario.last_name = data['apellidos']
+        # usuario.username = data['username']
+        # usuario.set_password(data['password1'])
+        # usuario.save()
+
+        usuario = form.save()
+        login(self.request, usuario, 'django.contrib.auth.backends.ModelBackend')
+
+        return super(Registro, self).form_valid(form)
